c85e995d12316e423e77b3a5f24215496d542d4d
suman-190 <149918576+suman-190@users.noreply.github.com>
2025-02-20 12:28:57 +0000

    "20-live-class"

---
+++ script.js
@@ -0,0 +1,236 @@
+// Array
+// 
+// arr[1]=5;
+// console.log(arr[1]);
+
+// let arr = [[6,14],[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]];
+
+
+// temp.push(arr[0][0]*arr[1][0]);
+// temp.push(arr[0][1]*arr[1][1]);
+// temp.push(arr[0][2]*arr[1][2]);
+// temp.push(arr[0][3]*arr[1][3]);
+// temp.push(arr[0][4]*arr[1][4]);
+// console.log(temp);
+// arr.push(temp);
+// console.log(arr);
+// console.log(arr);
+// console.log(arr[0][5]);
+// let arr2 = new Array(1, 2, 3, 4, 5);
+// arr.pop();
+// arr.push(6);
+// // console.log(arr.length);
+
+// object
+
+// let obj={
+//     name: 'John',
+//     age: 30,
+//     isMarried: false
+// }
+
+// obj.name = 'Suman';
+
+// console.log(obj.name);
+
+// const a = 32;
+// console.log(typeof a);
+// const b= String(a);
+// console.log(typeof b);
+
+// const fName= 'Suman';
+// const lName= 'Kumar';
+// const age = 30;
+// const isMarried = false;
+
+// // console.log('My name is ));
+// // console.log(`My name is ${fName}, I am ${age} years old and I am ${isMarried ? 'married' : 'not married'}`);
+
+// true ? console.log('Yes') : console.log('No');
+// if(age==30){
+//     console.log('Age is 30');}
+// else{
+
+//     console.log('Age is not 30');
+// }
+
+
+// loop
+
+
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// let j=0;
+// do{
+//     console.log(j);
+//     j++;
+// }
+// while(j<10);
+
+
+
+// const district= ["palpa", "syangja", "kaski", "tanahu", "gork"]
+
+// const province=["Bagmati", "Gandaki", "Lumbini", "Karnali", "Sudurpaschim"]
+
+// const village=["Dhakal", "Poudel", "Ghimire", "Pandey", "Karki"]
+
+// const newArr= district.concat(province);
+
+// const arr=[...district, ...province, ...village];
+
+// console.log(arr);
+
+
+
+
+///function 
+
+
+// const sum = (a,b) => {
+//     return a+b;
+// };
+
+// function sum(a,b){
+//    return a+b;
+// } 
+
+// const result=sum(arr);
+
+// console.log(result);
+
+//Break and continue
+
+// const arr=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // const temp=[];
+// console.log(arr.length)
+// for(let i=arr.length-1; i>0 ;i--){
+//     let x=arr[i];
+  
+    
+// }
+
+// console.log(this)
+
+// const obj={
+//     name: 'John',
+//     age: 30,
+//     isMarried: false,
+    
+
+// }
+
+
+// for(let y in obj){
+//     console.log(y);
+//     // console.log(obj[key]);
+// }
+
+// const arr=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for(let s in arr){
+//     console.log(arr[s]);
+// }
+
+// let btn=document.getElementById('btn')
+// console.log(btn);
+
+// btn.addEventListener('mouseenter', function(){
+//     console.log('Focus');
+// })
+
+// const para=document.querySelector('#para');
+// const inp=prompt('Enter your name');
+// // para.innerText='kjdihgfiufhbuih';
+
+// console.log(inp);
+
+// const conataines=[...con];
+// const con1=Array.from(con);
+// const first_ele=conataines[0];
+
+
+// console.log(con1);
+
+
+
+
+// setTimeout(()=>{    
+//     console.log('Hello');
+// }, 3000);
+
+// setInterval(()=>{    
+//     console.log('Interval Hello');
+// }, 1000);
+
+// console.log('This is Second Hello');
+
+// const form= document.querySelector('#form');
+// const name= document.querySelector('#name');
+// const email= document.querySelector('#email'); 
+
+// const handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log(e)
+//     console.log(name.value, email.value);
+//   }
+
+// const randomColor = () => {
+//     let val='O123456789ABCDEF';
+//     let color='#';
+//     for(let i=0; i<6; i++){
+//         color+=val[Math.floor(Math.random()*16)];
+//     }
+    
+// }
+// form.addEventListener('submit', handleSubmit);
+// console.log(Math.random()*16);
+
+
+
+// const square = (n) => {
+//     return n*n;
+// }
+
+// const cube = (n) => {    
+//     return n*n*n;
+// }
+
+
+
+// const sumOfSomeThing = (a, b, fn) => {
+    
+//     let sq1=fn(a);
+//     let sq2=fn(b);
+//     return sq1+sq2;
+// }
+
+// const res = sumOfSomeThing(2, 3, square); 
+// const res1=sumOfSomeThing(2,3,cube)
+
+// console.log(res1);
+
+
+
+// const obe ={
+//     courseName: 'MERN',
+//     duration: '6 months',   
+//     fee: "free"
+// }
+
+
+// const {courseName} = obe;
+
+// console.log(co);
+
+
+// const arr = ["palpa", "syangja", "kaski", "tanahu", "gork"]
+
+// const [ar1, b] = arr;
+
+// console.log(b);
+
+
